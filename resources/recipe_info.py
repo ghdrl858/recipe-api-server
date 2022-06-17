edd14041357df27a6f28032d6afbafda0f857952
ghdrl858 <ghdrl858@gmail.com>
2022-06-17 16:54:29 +0900

    2022-06-17

--- resources/recipe_info.py
+++ resources/recipe_info.py
@@ -55,3 +55,48 @@ class RecipeResource(Resource) :
 
         return {'result' : 'success' ,
                 'info' : result_list[0]}
+
+    # 데이터를 업데이트하는 API들은 put 함수를 사용한다.
+    def put(self, recipe_id) :
+
+        # body에서 전달된 데이터를 처리
+        data = request.get_json()
+
+        # DB 업데이트 실행코드
+        try :
+            # 데이터 update
+            # 1. DB에 연결
+            connection = get_connection()
+
+            # 2. 쿼리문 만들기
+            query = '''update recipe
+                    set name = %s, description = %s,
+                    cook_time = %s,
+                    directions = %s
+                    where id = %s;'''
+
+            # %s에 맞게 튜플로 작성한다.
+            record = (data['name'], data['description'], data['cook_time'], 
+                        data['directions'], recipe_id)
+            
+            # 3. 커서를 가져온다.
+            cursor = connection.cursor()
+
+            # 4. 쿼리문을 커서를 이용해서 실행한다.
+            cursor.execute(query, record)
+
+            # 5. 커넥션을 커밋해줘야 한다. -> DB에 영구적으로 반영하라는 뜻
+            connection.commit()
+
+            # 6. 자원 해제
+            cursor.close()
+            connection.close()
+
+        # 예외처리
+        except mysql.connector.Error as e :
+            print(e)
+            cursor.close()
+            connection.close()
+            return {'error' : str(e)}, 503
+
+        return {'result' : 'success'}, 200
